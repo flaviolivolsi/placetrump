5cdcf13bf1d828febb129c581fef0e2b467be570
Bjarke Walling <bwp@bwp.dk>
2016-04-05 18:48:30 +0200

    Initial image.

--- api/image.route.js
+++ api/image.route.js
@@ -1,11 +1,16 @@
 'use strict';
 
+var sharp   = require('sharp');
+var path    = require('path');
+var images  = path.join(__dirname, '../images');
+
 module.exports = [{
 
     method  : 'GET',
     path    : '/{width}/{height}',
     handler : function(request, reply) {
-        reply({});
+        var image = sharp(path.join(images, '1.jpg')).resize(request.params.width | 0, request.params.height | 0);
+        reply(image).type('image/jpeg');
     }
 
 }];
