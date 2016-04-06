ad12ae7aa6b4ad7024d0be074299f552d157c32d
Bjarke Walling <bwp@bwp.dk>
2016-04-06 08:21:56 +0200

    Support grayscale.

--- routes/image.route.js
+++ routes/image.route.js
@@ -10,6 +10,7 @@ module.exports = [{
     path    : '/{width}/{height}',
     handler : function(request, reply) {
         var image = sharp(path.join(images, '1.jpg')).resize(request.params.width | 0, request.params.height | 0);
+        if (request.query.gray) image = image.grayscale();
         reply(image).type('image/jpeg');
     }
 
