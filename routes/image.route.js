496a1fadb4f0ffe977d92bb0f02e84b55dcf0d5c
Bjarke Walling <bwp@bwp.dk>
2016-04-06 17:32:11 +0200

    Insane caching

--- routes/image.route.js
+++ routes/image.route.js
@@ -5,14 +5,23 @@ var sharp   = require('sharp');
 var path    = require('path');
 var images  = path.join(__dirname, '../images');
 
+var lastModified = new Date(2016, 3, 5, 2).toUTCString(); // "not modified" since project start
+
 module.exports = [{
 
     method  : 'GET',
     path    : '/{width}/{height}',
     handler : function(request, reply) {
-        var image = sharp(path.join(images, '1.jpg')).resize(request.params.width | 0, request.params.height | 0);
-        if (request.query.gray) image = image.grayscale();
-        reply(image).type('image/jpeg');
+        var width   = request.params.width;
+        var height  = request.params.height;
+        var gray    = request.query.gray;
+        var key     = ['placetrump', width, height, gray].join('_'); // etag caching
+
+        // load and scale image
+        var image = sharp(path.join(images, '1.jpg')).resize(width, height);
+        if (gray) image = image.grayscale();
+
+        reply(image).type('image/jpeg').etag(key).header('last-modified', lastModified);
     },
     config  : {
         validate : {
@@ -23,6 +32,10 @@ module.exports = [{
             query : {
                 gray    : joi.boolean().default(false, 'color image').optional()
             }
+        },
+        cache : {
+            expiresIn   : 30 * 24 * 60 * 60 * 1000, // 30 days
+            privacy     : 'public'
         }
     }
 
