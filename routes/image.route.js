6d134c914a523e9ee5d4d02c20e9a9130e23f677
Bjarke Walling <bwp@bwp.dk>
2016-04-06 17:09:38 +0200

    Input validation.

--- routes/image.route.js
+++ routes/image.route.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var joi     = require('joi');
 var sharp   = require('sharp');
 var path    = require('path');
 var images  = path.join(__dirname, '../images');
@@ -12,6 +13,17 @@ module.exports = [{
         var image = sharp(path.join(images, '1.jpg')).resize(request.params.width | 0, request.params.height | 0);
         if (request.query.gray) image = image.grayscale();
         reply(image).type('image/jpeg');
+    },
+    config  : {
+        validate : {
+            params : {
+                width   : joi.number().integer().min(1).max(3000).required(),
+                height  : joi.number().integer().min(1).max(3000).required()
+            },
+            query : {
+                gray    : joi.boolean().default(false, 'color image').optional()
+            }
+        }
     }
 
 }];
